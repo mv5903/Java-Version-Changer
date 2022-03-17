c966a1ab876e46d085627916ba335ef5be3a06c6
Matthew Vandenberg <mv5903@gmail.com>
2022-03-17 15:22:21 -0700

    Done

--- JavaVersionChanger-ConsoleApp/EnvironmentVariableManager.cs
+++ JavaVersionChanger-ConsoleApp/EnvironmentVariableManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 class EnvironmentVariableManager
 {
@@ -8,21 +9,32 @@ class EnvironmentVariableManager
         string[] PATHs = PATH.Split(';');
         foreach (string s in PATHs)
         {
-            if (s.Contains("Java"))
+            if (s.Contains("C:\\Program Files\\Java") || s.Contains("C:\\Program FIles (x86)\\Java"))
             {
                 string[] str = s.Split('\\');
                 string version = str[str.Length - 2];
+                Console.WriteLine(string.Join(";", str));
                 return "Your active Java Version is: " + version;
+            } else if (s.Contains("C:\\Program Files\\Common Files\\Oracle"))
+            {
+                // Included with some installations, not directly stored in path
+                FileVersionInfo myFileVersionInfo = FileVersionInfo.GetVersionInfo("C:\\Program Files\\Common Files\\Oracle\\Java\\javapath\\java.exe");
+                if (myFileVersionInfo.ProductName != null) return "Your active Java Version is: " + myFileVersionInfo.ProductName;
             }
         }
         return "You don't have an active version of Java.";
+    }
+
+    public static string GetFullPath()
+    {
+        return Environment.GetEnvironmentVariable("PATH");    
     }
 
     public static bool EditPath(JavaVersion newJavaVersion) 
     {
         // First, find items in path to remove
         List<string> pathsToRemove = new List<string>();
-        string currentPath = GetPath();
+        string currentPath = GetFullPath();
         List<string> eachPath = currentPath.Split(';').ToList<string>();
         foreach (string s in eachPath)
         {
@@ -34,14 +46,14 @@ class EnvironmentVariableManager
         // Delete these items
         foreach(string s in pathsToRemove)
         {
-            Console.WriteLine("Removing from path: " + s);
             eachPath.Remove(s);
         }
         // Add new path item to end of new path
         eachPath.Add(newJavaVersion.Path + "\\bin");
         string pathToWrite = string.Join(";", eachPath.ToArray());
-
+        Console.WriteLine("Writing to PATH...");
         Environment.SetEnvironmentVariable("PATH", pathToWrite, EnvironmentVariableTarget.Machine);
+        Console.WriteLine("Writing to JAVA_HOME...");
         Environment.SetEnvironmentVariable("JAVA_HOME", newJavaVersion.Path, EnvironmentVariableTarget.Machine);
         return true;
     }
